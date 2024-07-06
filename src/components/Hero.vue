1305519abd6eb00dcf21c1932f89d0353b0b054a
AuliaH <auliaku655@gmail.com>
2024-07-06 14:10:18 +0700

    https://github.com/auliahandayani/omg

---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+      <div class="col">
+        <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+                <h2><strong>Explore Fashion,</strong> <br>Anytime, Anywhere</h2>
+                <p>Ayo segera pilih dan pesan Fashion favorit Anda</p>
+                <button class="btn btn-lg btn-success">Pesan</button>
+            </div>
+        </div>
+      </div>
+      <div class="col">
+        <img src="../assets/images/hero.png" width="100%">
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HeroSection",
+};
+</script>
+
+<style>
+</style>
